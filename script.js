197531ed41da8a94bde273da89ce41ed8d7c52cd
Paul Niemczyk <mrpndev@gmail.com>
2021-01-21 20:32:51 -0500

    Added more notes

--- script.js
+++ script.js
@@ -5,4 +5,19 @@
 // - modified
 // file changed, but it has not yet been commited (ex: normal work, or saving a file)
 // - staging
-// 
+// files that are marked to go into the next commit snapshot (utilizing git add <file_name> or git add . to add every file in the folder)
+// - commit state
+// data safely stored in the local database (such as vm box, docker, computer, etc.)
+
+// to start version/track control
+// git init
+// creates new project
+// your project should never be inside another project folder in order to avoid inverse tree tracking issues
+
+// stage our files for a commit
+// git add <file>
+
+// use git status to check on file staging
+
+// to commit your files, use git commit -m "your message here"
+
